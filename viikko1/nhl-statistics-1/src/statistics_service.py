2d2a7e1b3183badd7c2801f53d5b1aa51fbbc4bd
Timur <tmandziev@gmail.com>
2024-11-04 21:53:48 +0200

    tehty tehtävä 15

--- viikko1/nhl-statistics-1/src/statistics_service.py
+++ viikko1/nhl-statistics-1/src/statistics_service.py
@@ -3,10 +3,10 @@ from player_reader import PlayerReader
 
 
 class StatisticsService:
-    def __init__(self):
-        reader = PlayerReader()
+    def __init__(self, player_reader):
+        self.reader = player_reader
 
-        self._players = reader.get_players()
+        self._players = self.reader.get_players()
 
     def search(self, name):
         for player in self._players:
